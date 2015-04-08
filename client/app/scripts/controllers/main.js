3041b37cd6a8fb333f6144bac508463750188e98
Erin Lodes <erinlodes@Erin-Lodess-MacBook-Pro.local>
2015-04-07 22:07:22 -0500

    Initial Commmit

---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    //$scope.isActive = function (viewLocation) {
+    //  var active = (viewLocation === $location.path());
+    //  return active;
+    //};
+  });
---
+++ server/app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+
+
+
+var app = express();
+
+// view engine setup
+app.use(express.static(path.join(__dirname, 'public')));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+/**
+ * Development Settings
+ */
+if (app.get('env') === 'development') {
+
+    // This will change in production since we'll be using the dist folder
+    // This covers serving up the index page
+    app.use(express.static(path.join(__dirname, '../client/.tmp')));
+    app.use(express.static(path.join(__dirname, '../client/app')));
+
+}
+
+/**
+ * Production Settings
+ */
+if (app.get('env') === 'production') {
+
+    // changes it to use the optimized version for production
+    app.use(express.static(path.join(__dirname, '/dist')));
+
+}
+
+/**
+ * Routes
+ */
+var router = require('./router')(app);
+
+// Error Handling
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+});
+
+module.exports = app;
+
+
+/**
+ * Routes
+ */
+var router = require('./router')(app);
+
+// Error Handling
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+});
+
+module.exports = app;
