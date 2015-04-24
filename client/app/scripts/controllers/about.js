caff742d7dcab28a99546b870728b5b75b661f94
elodes <erin.lodes@gmail.com>
2015-04-23 22:53:52 -0500

    Centered frontlinks, tried to make them clickable (fail), fonts, containers

--- client/app/scripts/controllers/about.js
+++ client/app/scripts/controllers/about.js
@@ -21,12 +21,12 @@ angular.module('clientApp')
         function() {
           $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
           $(this).toggleClass('open');
-          $('b', this).toggleClass("caret caret-up");
+          $('b', this).toggleClass;
         },
         function() {
           $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
           $(this).toggleClass('open');
-          $('b', this).toggleClass("caret caret-up");
+          $('b', this).toggleClass;
         });
     });
 
--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $location) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -20,14 +20,18 @@ angular.module('clientApp')
         function() {
           $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
           $(this).toggleClass('open');
-          $('b', this).toggleClass("caret caret-up");
+          $('b', this).toggleClass;
         },
         function() {
           $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
           $(this).toggleClass('open');
-          $('b', this).toggleClass("caret caret-up");
+          $('b', this).toggleClass;
         });
     });
+
+    $scope.goto=function(path){
+      $location.path(path);
+    };
 
     //$scope.isActive = function (viewLocation) {
     //  var active = (viewLocation === $location.path());
