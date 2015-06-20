be64ddf775d0a27334427eff4457ef080d892730
elodes <erin.lodes@gmail.com>
2015-06-20 17:00:53 -0500

    contact form

--- client/app/scripts/controllers/about.js
+++ client/app/scripts/controllers/about.js
@@ -15,19 +15,34 @@ angular.module('clientApp')
       'Karma'
     ];
 
+      $scope.greeting = 'Hola!';
 
-    $(function(){
-      $(".dropdown").hover(
-        function() {
-          $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
-          $(this).toggleClass('open');
-          $('b', this).toggleClass;
-        },
-        function() {
-          $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
-          $(this).toggleClass('open');
-          $('b', this).toggleClass;
-        });
+      var dotenv = require('dotenv');
+      dotenv.load();
+
+      var sendgrid_username = process.env.SENDGRID_USERNAME;
+      var sendgrid_password = process.env.SENDGRID_PASSWORD;
+      var to = process.env.TO;
+
+      var sendgrid = require('sendgrid')(sendgrid_username, sendgrid_password);
+      var email = new sendgrid.Email();
+
+
+      email.addTo(to);
+      email.setFrom(to);
+      email.setSubject('[sendgrid-php-example] Owl');
+      email.setText('Owl are you doing?');
+      email.setHtml('<strong>%how% are you doing?</strong>');
+      email.addSubstitution("%how%", "Owl");
+      email.addHeader('X-Sent-Using', 'SendGrid-API');
+      email.addHeader('X-Transport', 'web');
+
+      sendgrid.send(email, function (err, json) {
+        if (err) {
+          return console.error(err);
+        }
+        console.log(json);
+      });
+
     });
 
-  });
---
+++ client/app/scripts/controllers/clarity.js
@@ -0,0 +1,34 @@
+'use strict';
+var app = angular.module('clientApp', []);
+
+app.controller('ClarityCtrl', ['$scope', function($scope){
+  $scope.greeting = 'Hola!';
+
+    var dotenv = require('dotenv');
+    dotenv.load();
+
+    var sendgrid_username = process.env.SENDGRID_USERNAME;
+    var sendgrid_password = process.env.SENDGRID_PASSWORD;
+    var to = process.env.TO;
+
+    var sendgrid = require('sendgrid')(sendgrid_username, sendgrid_password);
+    var email = new sendgrid.Email();
+
+
+    email.addTo(to);
+    email.setFrom(to);
+    email.setSubject('[sendgrid-php-example] Owl');
+    email.setText('Owl are you doing?');
+    email.setHtml('<strong>%how% are you doing?</strong>');
+    email.addSubstitution("%how%", "Owl");
+    email.addHeader('X-Sent-Using', 'SendGrid-API');
+    email.addHeader('X-Transport', 'web');
+
+    sendgrid.send(email, function (err, json) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log(json);
+    });
+
+  });
